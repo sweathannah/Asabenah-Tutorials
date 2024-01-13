74e2676d19afc73b92878c9e41de091f2faa6e55
unknown <sultanatmbashir@gmail.com>
2024-01-13 21:25:44 +0100

    first commit

---
+++ Day1/Variable.js
@@ -0,0 +1,66 @@
+
+
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*comments can make code 
+readable, easy to reuse
+and informative*/
+
+let nickName = 'Arabgirl';
+let isFemale = true;
+let height; // have not known my height yet
+let  gender  = null; //not known my gender too
+
+console.log(nickName);
+console.log(isFemale);
+console.log(height);
+console.log(gender);
+
+console.log(typeof nickName);
+console.log(typeof isFemale );
+console.log(typeof height );
+console.log(typeof gender );
+
+let color = null;
+let bestFood = null;
+let school = null;
+let weight = null;
+
+console.log(color );
+console.log(bestFood);
+console.log(school );
+console.log(weight );
+
+let hubbby = "coding";
+let car = "suv";
+let bestFriend ="Fatima";
+let phone = "Samsung"
+
+console.log( hubbby );
+console.log( car );
+console.log(bestFriend );
+console.log( phone );
+
+let firstName = 'Sultanat';
+let lastName = 'Bashir';
+let country = 'Nigeria';
+let age = 16;
+let isMarried = false;
+
+console.log( firstName );
+console.log( lastName );
+console.log( country );
+console.log( age );
+console.log( isMarried );
+
+let name="sultanat", job="Student", lives="Nigeria";
+console.log(name, job, lives);
+
+let myAge = "I am 16 years old.";
+console.log(myAge);
+let yourAge = "You are 30 years old.";
+console.log(yourAge)
+
+ 
---
+++ Day1/script.js
@@ -0,0 +1,35 @@
+
+console.log("Hello", "World", "!")
+console.log("Happy", "New", "Year", 2024 )
+console.log("Welcome", "To", 30, "Days", "Of", "Javascript")
+
+
+console.log(2 + 3) // Addition
+console.log(3 - 2) // Subtraction
+console.log(2 * 3) // Multiplication
+console.log(3 / 2) // Division
+console.log(3 % 2) // Modulus - finding remainder
+console.log(3 ** 2) // Exponentiation 3 ** 2 == 3 * 3
+
+
+// Declaring different variables of different data types
+let firstName = 'Sultanat' // first name of a person
+let lastName = 'Bashir' // last name of a person
+let country = 'Nigeria' // country
+let city = 'Kwara' // capital city
+let age = 16 // age in years
+let isMarried = false
+
+// or
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name="sultanat", job="Student", lives="Nigeria";
+console.log(name, job, lives)
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Declaring variables with number values
+// let age = 100 // age in years
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
