da8e77495cce246921afb8cac26c71e14375c29b
unknown <sultanatmbashir@gmail.com>
2024-02-05 22:23:53 +0100

    arrays

---
+++ Day5/exercise1.js
@@ -0,0 +1,10 @@
+
+const array = Array ();
+console.log(array);
+
+let classMates = ['Basheeroh', 'Halimah', 'Hadizat', 'Fatimah', 'Aminat']
+console.log(classMates);    //an array with more than 5 number of elements
+console.log(classMates.length); // length of your array
+console.log(classMates[0]); //the first item
+console.log(classMates[2]); //the first item
+console.log(classMates[classMates.length - 1]);
